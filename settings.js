d41d5b4323647abb6641599df3aba0fb870169c5
Maxime Leriche <maxime.leriche@cpe.fr>
2024-05-25 20:17:27 +0200

    chore: settings cleanup

--- settings.js
+++ settings.js
@@ -28,11 +28,13 @@ class Settings {
         this.setCategoryDescription(`General information about MixendMod`);
         this.setCategoryDescription("Fishing", `Fishing related settings`);
 
-        this.addDependency(`${TABname}Enable party transfer`, "Enable party commands");
-        this.addDependency(`${TABname}Enable party warp`, "Enable party commands");
+        // 1. Catch session GUI
+        this.addDependency(`Catch session GUI times`, "Catch session GUI");
+        this.addDependency(`Catch session GUI percentages`, "Catch session GUI");
+        this.addDependency(`Catch session GUI scope`, "Catch session GUI");
+        this.addDependency(`Catch session GUI fishing type`, "Catch session GUI");
 
-        this.addDependency(`${TABname}Double hook message`, "Double hook");
-
+        // 2. Fishing GUI
         this.addDependency(`${TABname}Mythic creature count`, "Fishing GUI");
         this.addDependency(`${TABname}Bobber`, "Fishing GUI");
         this.addDependency(`${TABname}Active pet`, "Fishing GUI");
@@ -41,26 +43,32 @@ class Settings {
         this.addDependency(`${TABname}Catching rate window length`, "Fishing GUI");
         this.addDependency(`${TABname}Average mode`, "Fishing GUI");
 
+        // 3. Crimson Catch
+        this.addDependency(`${TABname}Lord Jawbus catch`, "Lord Jawbus settings");
+        this.addDependency(`${TABname}Lord Jawbus sound`, "Lord Jawbus settings");
+        this.addDependency(`${TABname}Lord Jawbus alert`, "Lord Jawbus settings");
+
+        this.addDependency(`${TABname}Plhlegblast catch`, "Plhlegblast settings");
+        this.addDependency(`${TABname}Plhlegblast sound`, "Plhlegblast settings");
+        this.addDependency(`${TABname}Plhlegblast alert`, "Plhlegblast settings");
+
+        this.addDependency(`${TABname}Thunder catch`, "Thunder settings");
+        this.addDependency(`${TABname}Thunder sound`, "Thunder settings");
+        this.addDependency(`${TABname}Thunder alert`, "Thunder settings");
+
+        // 5. Other
+        this.addDependency(`${TABname}Double hook message`, "Double hook");
+
+        // Other 
+        this.addDependency(`${TABname}Enable party transfer`, "Enable party commands");
+        this.addDependency(`${TABname}Enable party warp`, "Enable party commands");
+
         this.addDependency(`${TABname}Cap threshold`, "Ping worm cap");
         this.addDependency(`${TABname}Radioactive Vial`, "Magic Find party ping");
         this.addDependency(`${TABname}Lucky Clover Core`, "Magic Find party ping");
         this.addDependency(`${TABname}Deep Sea Orb`, "Magic Find party ping");
         this.addDependency(`${TABname}Daedalus Stick`, "Magic Find party ping");
 
-        // LORD JAWBUS
-        this.addDependency(`${TABname}Lord Jawbus catch`, "Lord Jawbus settings");
-        this.addDependency(`${TABname}Lord Jawbus sound`, "Lord Jawbus settings");
-        this.addDependency(`${TABname}Lord Jawbus alert`, "Lord Jawbus settings");
-
-        // PLHLEGBLAST
-        this.addDependency(`${TABname}Plhlegblast catch`, "Plhlegblast settings");
-        this.addDependency(`${TABname}Plhlegblast sound`, "Plhlegblast settings");
-        this.addDependency(`${TABname}Plhlegblast alert`, "Plhlegblast settings");
-
-        // THUNDER
-        this.addDependency(`${TABname}Thunder catch`, "Thunder settings");
-        this.addDependency(`${TABname}Thunder sound`, "Thunder settings");
-        this.addDependency(`${TABname}Thunder alert`, "Thunder settings");
 
         // VANQUISHER
         this.addDependency(`${TABname}Vanquisher sound`, "Vanquisher settings");
@@ -88,7 +96,7 @@ class Settings {
     enablePartyWarp = true;
     @CheckboxProperty({
         name: `${TABname}Enable party transfer`,
-        description: `Enable !pt <PlayerName> to transfer the party to the specified player`,
+        description: `Enable !pt <PlayerName> to transfer the party to the specified player\n!pt and !ptme also available`,
         category: "General",
         subcategory: "Party commands",
     })
@@ -100,12 +108,116 @@ class Settings {
 
     //#region Fishing
 
-    //#region Lord Jawbus
+    //#region 1. Catch session GUI
+    @SwitchProperty({
+        name: "Catch session GUI",
+        description: "Display catch session for crimson isle fishing. /mixgui session to move",
+        category: "Fishing",
+        subcategory: "1. Catch session GUI",
+    })
+    catchSession = true;
+
+    @SwitchProperty({
+        name: "Catch session GUI times",
+        description: "Display times since last sc in the catch session GUI",
+        category: "Fishing",
+        subcategory: "1. Catch session GUI",
+    })
+    catchSessionTime = true;
+
+    @SwitchProperty({
+        name: "Catch session GUI percentages",
+        description: "Display percentages in the catch session GUI",
+        category: "Fishing",
+        subcategory: "1. Catch session GUI",
+    })
+    catchSessionPercentage = true;
+
+    @SwitchProperty({
+        name: "Catch session GUI scope",
+        description: "O: current | I: global",
+        category: "Fishing",
+        subcategory: "1. Catch session GUI",
+    })
+    catchSessionScope = false;
+
+    @SwitchProperty({
+        name: "Catch session GUI fishing type",
+        description: "O: LAVA | I: WATER ",
+        category: "Fishing",
+        subcategory: "1. Catch session GUI",
+    })
+    catchSessionFishingType = false;
+    //#endregion 1. Catch session GUI
+
+    //#region 2. Fishing GUI
+    @SwitchProperty({
+        name: "Fishing GUI",
+        description: "Display fishing GUI. /mixgui fish to move",
+        category: "Fishing",
+        subcategory: "2. Fishing GUI"
+    })
+    fishingGUI = true;
+
+    @CheckboxProperty({
+        name: `${TABname}Mythic creature count`,
+        description: `Display creature catch since mythic. Jawbus & Thunder`,
+        category: "Fishing",
+        subcategory: "2. Fishing GUI"
+    })
+    fishingGUIMythic = true;
+
+    @CheckboxProperty({
+        name: `${TABname}Bobber`,
+        description: `Display bobber count. Requires bobbers to be rendered in your game`,
+        category: "Fishing",
+        subcategory: "2. Fishing GUI"
+    })
+    fishingGUIBobbers = true;
+
+    @CheckboxProperty({
+        name: `${TABname}Active pet`,
+        description: `Display active pet. Requires to summon your pet again, or trigger a pet rule to activate`,
+        category: "Fishing",
+        subcategory: "2. Fishing GUI"
+    })
+    fishingGUIPet = true;
+
+    @CheckboxProperty({
+        name: `${TABname}Catch rate`,
+        description: `Display sea creature catch rate. Value is sea creature per minutes over the last 20 minutes by default.`,
+        category: "Fishing",
+        subcategory: "2. Fishing GUI"
+    })
+    fishingGUIRate = true;
+
+    @SliderProperty({
+        name: `${TABname}Catching rate window length`,
+        description: `Set window duration for moving average computation (minutes)`,
+        category: "Fishing",
+        subcategory: "2. Fishing GUI",
+        min: 1,
+        max: 60
+    })
+    fishingGUILength = 5;
+
+    @SwitchProperty({
+        name: `${TABname}Average mode`,
+        description: `ON: per hour, OFF: per minute`,
+        category: "Fishing",
+        subcategory: "2. Fishing GUI"
+    })
+    fishingGUIAvgMode = true;
+    //#endregion 2. Fishing GUI
+
+    //#region 3. Crimson catch
+    // Lord Jawbus
+
     @SwitchProperty({
         name: `Lord Jawbus settings`,
         description: `Turn ON/OFF Lord Jawbus related settings`,
         category: "Fishing",
-        subcategory: "1. Crimson catch",
+        subcategory: "3. Crimson catch",
     })
     jawbusSettings = true;
 
@@ -113,7 +225,7 @@ class Settings {
         name: `${TABname}Lord Jawbus catch`,
         description: "Party ping on catch",
         category: "Fishing",
-        subcategory: "1. Crimson catch",
+        subcategory: "3. Crimson catch",
     })
     jawbusCatch = true;
 
@@ -121,7 +233,7 @@ class Settings {
         name: `${TABname}Lord Jawbus sound`,
         description: "Sound alert on detection",
         category: "Fishing",
-        subcategory: "1. Crimson catch",
+        subcategory: "3. Crimson catch",
     })
     jawbusSoundAlert = true;
 
@@ -129,17 +241,17 @@ class Settings {
         name: `${TABname}Lord Jawbus alert`,
         description: "Screen alert on detection",
         category: "Fishing",
-        subcategory: "1. Crimson catch",
+        subcategory: "3. Crimson catch",
     })
     jawbusScreenAlert = true;
-    //#endregion Lord Jawbus
 
-    //#region Thunder
+    // Thunder
+
     @SwitchProperty({
         name: `Thunder settings`,
         description: `Turn ON/OFF Thunder related settings`,
         category: "Fishing",
-        subcategory: "1. Crimson catch",
+        subcategory: "3. Crimson catch",
     })
     thunderSettings = true;
 
@@ -147,7 +259,7 @@ class Settings {
         name: `${TABname}Thunder catch`,
         description: "Party ping on catch",
         category: "Fishing",
-        subcategory: "1. Crimson catch",
+        subcategory: "3. Crimson catch",
     })
     thunderCatch = true;
 
@@ -155,7 +267,7 @@ class Settings {
         name: `${TABname}Thunder sound`,
         description: "Sound alert on detection",
         category: "Fishing",
-        subcategory: "1. Crimson catch",
+        subcategory: "3. Crimson catch",
     })
     thunderSoundAlert = true;
 
@@ -163,17 +275,17 @@ class Settings {
         name: `${TABname}Thunder alert`,
         description: "Screen alert on detection",
         category: "Fishing",
-        subcategory: "1. Crimson catch",
+        subcategory: "3. Crimson catch",
     })
     thunderScreenAlert = true;
-    //#endregion Thunder
 
-    //#region Plhlegblast
+    // Plhlegblast
+
     @SwitchProperty({
         name: `Plhlegblast settings`,
         description: `Turn ON/OFF Plhlegblast related settings`,
         category: "Fishing",
-        subcategory: "1. Crimson catch",
+        subcategory: "3. Crimson catch",
     })
     plhlegblastSettings = true;
 
@@ -181,7 +293,7 @@ class Settings {
         name: `${TABname}Plhlegblast catch`,
         description: "Party ping on catch",
         category: "Fishing",
-        subcategory: "1. Crimson catch",
+        subcategory: "3. Crimson catch",
     })
     plhlegblastCatch = true;
 
@@ -189,7 +301,7 @@ class Settings {
         name: `${TABname}Plhlegblast sound`,
         description: "Sound alert on detection",
         category: "Fishing",
-        subcategory: "1. Crimson catch",
+        subcategory: "3. Crimson catch",
     })
     plhlegblastSoundAlert = true;
 
@@ -197,59 +309,17 @@ class Settings {
         name: `${TABname}Plhlegblast alert`,
         description: "Screen alert on detection",
         category: "Fishing",
-        subcategory: "1. Crimson catch",
+        subcategory: "3. Crimson catch",
     })
     plhlegblastScreenAlert = true;
-    //#endregion Plhlegblast
+    //#endregion 3. Crimson catch
 
-    //#region catch session
-    @SwitchProperty({
-        name: "Catch session GUI",
-        description: "Display catch session for crimson isle fishing. /mixgui session to move",
-        category: "Fishing",
-        subcategory: "Catch session",
-    })
-    catchSessionGui = true;
-
-    @SwitchProperty({
-        name: "Catch session GUI times",
-        description: "Display times since last sc in the catch session GUI",
-        category: "Fishing",
-        subcategory: "Catch session",
-    })
-    catchSessionGuiTime = true;
-
-    @SwitchProperty({
-        name: "Catch session GUI percentages",
-        description: "Display percentages in the catch session GUI",
-        category: "Fishing",
-        subcategory: "Catch session",
-    })
-    catchSessionGuiPercentage = true;
-
-    @SwitchProperty({
-        name: "Global / current session recap",
-        description: "O: current | I: global",
-        category: "Fishing",
-        subcategory: "Catch session",
-    })
-    statMode = false;
-
-    @SwitchProperty({
-        name: "WATER / LAVA fish session",
-        description: "O: LAVA | I: WATER ",
-        category: "Fishing",
-        subcategory: "Catch session",
-    })
-    statVersion = true;
-    //#endregion catch session
-
-    //#region catch party ping
+    //#region 4. Other catch
     @CheckboxProperty({
         name: `${TABname}Carrot King`,
         description: `Enable Carrot King catch party ping`,
         category: "Fishing",
-        subcategory: "2. Other catch",
+        subcategory: "4. Other catch",
     })
     sendCarrotKingCatch = true;
 
@@ -257,7 +327,7 @@ class Settings {
         name: `${TABname}Sea Emperor`,
         description: `Enable Sea Emperor catch party ping`,
         category: "Fishing",
-        subcategory: "2. Other catch",
+        subcategory: "4. Other catch",
     })
     sendSeaEmperorCatch = true;
 
@@ -265,7 +335,7 @@ class Settings {
         name: `${TABname}Phantom Fisherman`,
         description: `Enable Phantom Fisherman catch party ping`,
         category: "Fishing",
-        subcategory: "2. Other catch",
+        subcategory: "4. Other catch",
     })
     sendPhantomFishermanCatch = true;
 
@@ -273,17 +343,18 @@ class Settings {
         name: `${TABname}Grim Reaper`,
         description: `Enable Grim Reaper catch party ping`,
         category: "Fishing",
-        subcategory: "2. Other catch",
+        subcategory: "4. Other catch",
     })
     sendGrimReaperCatch = true;
-    //#endregion catch party ping
+    //#endregion 4. Other catch
 
-    //#region double hook
+    //#region 5. Other
+
     @SwitchProperty({
         name: "Double hook",
         description: "Enable double hook party ping",
         category: "Fishing",
-        subcategory: "Double Hook"
+        subcategory: "5. Other"
     })
     sendDoubleHook = true;
 
@@ -291,81 +362,22 @@ class Settings {
         name: `${TABname}Double hook message`,
         description: `Custom message sent to the party`,
         category: "Fishing",
-        subcategory: "Double Hook"
+        subcategory: "5. Other"
     })
     doubleHookMsg = "Noot noot >o<";
-    //#endregion double hook
 
-    //#region fishing gui
     @SwitchProperty({
-        name: "Fishing GUI",
-        description: "Display fishing GUI. /mixgui fish to move",
-        category: "Fishing",
-        subcategory: "Fishing GUI"
-    })
-    guiEnable = true;
-
-    @CheckboxProperty({
-        name: `${TABname}Mythic creature count`,
-        description: `Display creature catch since mythic. Jawbus & Thunder`,
-        category: "Fishing",
-        subcategory: "Fishing GUI"
-    })
-    guiMythicCount = true;
-
-    @CheckboxProperty({
-        name: `${TABname}Bobber`,
-        description: `Display bobber count. Requires bobbers to be rendered in your game`,
-        category: "Fishing",
-        subcategory: "Fishing GUI"
-    })
-    guiBobberCount = true;
-
-    @CheckboxProperty({
-        name: `${TABname}Active pet`,
-        description: `Display active pet. Requires to summon your pet again, or trigger a pet rule to activate`,
-        category: "Fishing",
-        subcategory: "Fishing GUI"
-    })
-    guiActivePet = true;
-
-    @CheckboxProperty({
-        name: `${TABname}Catch rate`,
-        description: `Display sea creature catch rate. Value is sea creature per minutes over the last 20 minutes by default.`,
-        category: "Fishing",
-        subcategory: "Fishing GUI"
-    })
-    guiCatchRate = true;
-    //#endregion fishing gui
-
-    //#region catching rate settings
-    @SwitchProperty({
-        name: "Party ping catch rate display",
+        name: "Party ping catch rate display mode",
         description: "Change the catch rate display\nOFF: 32 in 5m | ON: 32 at 750/h",
         category: "Fishing",
-        subcategory: "GUI Catch rate",
+        subcategory: "5. Other",
     })
     catchPingMode = true;
 
-    @SliderProperty({
-        name: `${TABname}Catching rate window length`,
-        description: `Set window duration for moving average computation (minutes)`,
-        category: "Fishing",
-        subcategory: "GUI Catch rate",
-        min: 1,
-        max: 60
-    })
-    scRateWindowMin = 5;
-
-    @SwitchProperty({
-        name: `${TABname}Average mode`,
-        description: `ON: per hour, OFF: per minute`,
-        category: "Fishing",
-        subcategory: "GUI Catch rate"
-    })
-    guiCatchRateMode = true;
     //#endregion catching rate settings
+
     //#endregion Fishing
+
     //#region Crimson Isle
     @SwitchProperty({
         name: `Miniboss GUI`,
